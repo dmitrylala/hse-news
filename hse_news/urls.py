d515f7d82bd802ad4b5e962239343612f4d7d5bb
dmitrylala <dim.nesterov2015@gmail.com>
2023-10-04 23:15:08 +0300

    fix issues

--- hse_news/urls.py
+++ hse_news/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("news.urls")),
     path("news_navig/", include("news_navig.urls")),
-    path("news/", include("news.urls")),
     path("algo/", include("algo.urls")),
     path("admin/", admin.site.urls),
 ]
