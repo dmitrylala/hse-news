650f5c935547d5ec98b29f3d8ab2d1ffd5ab1594
dmitrylala <dim.nesterov2015@gmail.com>
2023-10-04 11:07:44 +0300

    init django project

---
+++ hse_news/wsgi.py
@@ -0,0 +1,15 @@
+"""WSGI config for hse_algo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hse_algo.settings")
+
+application = get_wsgi_application()
